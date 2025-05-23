b7092b96f47408b745774bb2013d7777631233f8
Davi Cagnato <113918935+cagnato@users.noreply.github.com>
2025-05-22 22:14:30 -0300

    Update ex1.py

--- Lista_matriz_RA/ex1.py
+++ Lista_matriz_RA/ex1.py
@@ -1,10 +1,13 @@
-matriz = [
-    [1, 0, 0, 0, 0],
-    [0, 1, 0, 0, 0],
-    [0, 0, 1, 0, 0],
-    [0, 0, 0, 1, 0],
-    [0, 0, 0, 0, 1]
-]
+matriz = []
 
 for i in range(5):
-    print(matriz[i])
+    linha = []
+    for j in range(5):
+        if i == j:
+            linha.append(1)
+        else:
+            linha.append(0)
+    matriz.append(linha)
+
+for linha in matriz:
+    print(linha)
